f2b127bb959ce6dc35eb56554bf2f017523247c6
Gopal Srivastava <71832523+gnsrivastava@users.noreply.github.com>
2025-09-08 11:26:59 -0500

    Create CreatediamondDB.sh

---
+++ ProcessFasta/CreatediamondDB.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+#Steps:
+
+#Prepare a list of species (fasta files) in a directory.
+#For each species, create a Diamond database.
+#Then for each pair (i, j) where i != j, run the two Diamond searches and compute the average.

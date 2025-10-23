37e351b3944c594cd44f1853227ce20983684b8c
Gopal Srivastava <71832523+gnsrivastava@users.noreply.github.com>
2025-10-23 14:48:48 -0500

    Enhance makeblastdb.sh to support Diamond makedb
    Added functionality to create a database using Diamond for multiple FASTA files.

--- ProteomeBasedSpeciesSimilarity/TestScripts/ProcessFasta/makeblastdb.sh
+++ ProteomeBasedSpeciesSimilarity/TestScripts/ProcessFasta/makeblastdb.sh
@@ -5,3 +5,10 @@ file={}
   $makeblastdb -in "$file" -dbtype prot -out "$base_name/${base_name}_db"
 '
 
+# Script for Diamond makedb
+mkdir OUTDIR
+ls INPUTDIR/*.faa | parallel --jobs 20 '
+  file={}
+  base_name=$(basename "$file" .faa | cut -d"_" -f1)
+  /INSTALLATIONLOCATION/diamond makedb --in {} -d ./OUTDIR/$base_name
+'
